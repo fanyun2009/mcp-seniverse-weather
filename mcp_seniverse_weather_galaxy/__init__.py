7b95bd9ee5f7dd6ed96090faed999def583becf4
fanyun2009 <gitee_fanyun2009@126.com>
2025-10-24 12:35:51 +0800

    调用代码：框架：添加服务器状态追踪；地理位置缓存

--- mcp_seniverse_weather_galaxy/__init__.py
+++ mcp_seniverse_weather_galaxy/__init__.py
@@ -2,7 +2,9 @@ from mcp_seniverse_weather_galaxy.server import mcp
 
 def main():
     """Entry point for the weather mcp server"""
-    mcp.run(transport="stdio")
+    # mcp.run(transport="stdio")
+    import asyncio
+    asyncio.run(main())
 
 if __name__ == '__main__':
     main()
